8468470cec0474c4cf4a0b364f385cfb08bb68fb
Yuehe Guo <guoyuehe98@gmail.com>
2023-09-20 18:26:50 -0700

    change button arragement

---
+++ Assets/Scripts/SwitchScene.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class SwitchScene : MonoBehaviour
+{
+    public Button playButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playButton.onClick.AddListener(PlayGame);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void PlayGame()
+    {
+        // Load the gameplay scene when the play button is clicked.
+        SceneManager.LoadScene("SampleScene");
+    }
+}
