ba81505356cec3c678c8adb6fe3cec4dbd3a5581
Yuehe Guo <guoyuehe98@gmail.com>
2023-09-20 22:43:52 -0700

    get ememy color at runtime

---
+++ Assets/Scripts/SelectEnemy.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectEnemy : MonoBehaviour
+{
+    public Color enemyColor;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SpriteRenderer renderer = GetComponent<SpriteRenderer>();
+        if(renderer.isVisible)
+        {
+            // Perform the selection logic here.
+            Select();
+        }
+    }
+    private void Select()
+    {
+        SpriteRenderer renderer = GetComponent<SpriteRenderer>();
+
+        if (renderer == null)
+        {
+            return;
+        }
+        
+        // Get the color property from the material.
+        enemyColor = renderer.color;
+
+        // Use the objectColor as needed.
+        Debug.Log("Prefab color: " + enemyColor);
+    }
+}
